3f85223765d8849255ab5a81f6f584b04a12578e
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-09-14 17:48:28 -0300

    estilizacao

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttonForm = document.getElementById('button-form-validate');
 
 function validateEmailPassword() {
-  const inputEmail = document.getElementById('input-email').value;
+  const inputEmail = document.getElementById('input-email-loguin').value;
   const inputPassword = document.getElementById('input-password').value;
   if (inputEmail === 'tryber@teste.com' && inputPassword === '123456') {
     return window.alert('Olá, Tryber!');
