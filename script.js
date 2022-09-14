72c1aabe4f606bfd6dd1281188de4946cfb4b664
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-14 17:46:18 -0300

    Corrigindo requisito 18

--- script.js
+++ script.js
@@ -14,6 +14,6 @@ function desableButton() {
   const inputCheckbox = document.getElementById('agreement');
   if (inputCheckbox.checked) {
     document.getElementById('submit-btn').disabled = false;
-  }
+  } else document.getElementById('submit-btn').disabled = true;
 }
 document.getElementById('agreement').addEventListener('click', desableButton);
